261e738873be41c32b1f63e16451a19d709d89d1
cccaaannn <can.kurt.aa@gmail.com>
2024-08-25 00:25:58 +0300

    404 fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +12,12 @@ export default defineConfig({
     '**/*.gltf',
     '**/*.glb'
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        contact: resolve(__dirname, "404.html"),
+      },
+    },
+  },
 })
